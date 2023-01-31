7a731a684473f3380dc759847fe420c7013f2524
prabhatjoshi <joshi.prabhat.dev@gmail.com>
2023-01-31 13:21:34 +0530

    remove eslint warning

--- src/components/EmployeeTable.tsx
+++ src/components/EmployeeTable.tsx
@@ -133,6 +133,7 @@ function List() {
                                                         </div>
                                                     )
                                                 }
+                                                return null
                                             })
 
                                         }
